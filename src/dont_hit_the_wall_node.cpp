da51f938b28ba32d490ba1735b8827b2abc6cc1d
Christian Wendt <christian.wendt@francor.de>
2024-01-17 09:15:30 +0100

    Save at workday end.

---
+++ src/dont_hit_the_wall_node.cpp
@@ -0,0 +1,19 @@
+//################################################################
+//# Copyright (C) 2023, EduArt Robotik GmbH, All rights reserved.#
+//# Further information can be found in the LICENSE file.        #
+//################################################################
+
+#include <rclcpp/node.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+class DontHitTheWallNode : public rclcpp::Node
+{
+public:
+  DontHitTheWallNode()
+    : rclcpp::Node("don't hit the wall")
+  {
+
+  }
+
+  
+};
